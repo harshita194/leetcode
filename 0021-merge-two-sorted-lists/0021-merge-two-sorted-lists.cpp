184ea79e79425ae55aaf76c1e4d703702b3851ef
harshita194 <156418698+harshita194@users.noreply.github.com>
2025-05-15 12:37:43 +0530

    Time: 0 ms (100%), Space: 19.6 MB (28.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -14,7 +14,7 @@ private:
         if(list1->next == NULL){
             list1->next = list2;
             return list1;
-        }    
+        }
 
         ListNode* curr1 = list1;
         ListNode* next1 = curr1->next;
@@ -22,22 +22,19 @@ private:
         ListNode* next2 = curr2->next;
 
         while(next1 != NULL && curr2 != NULL){
-            if((curr2->val >= curr1->val) && (curr2->val <= next1->val)){
-                // add node in between the first list
+            if((curr2->val >= curr1->val )&& (curr2->val <= next1->val)){
                 curr1->next = curr2;
-                next2 = curr2->next; 
+                next2 = curr2->next;
                 curr2->next = next1;
-                // update pointers
                 curr1 = curr2;
                 curr2 = next2;
             }
             else{
                 curr1 = next1;
                 next1 = next1->next;
-            
                 if(next1 == NULL){
                     curr1->next = curr2;
-                    return list1; 
+                    return list1;
                 }
             }
         }
@@ -49,10 +46,10 @@ public:
         if(list2 == NULL) return list1;
 
         if(list1->val <= list2->val){
-            return solve(list1, list2);
+            return solve(list1,list2);
         }
         else{
-            return solve(list2, list1);
+            return solve(list2,list1);
         }
     }
 };
