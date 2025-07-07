f89709a1cc753b732c361beb35593823a87e4e9d
Harshita Kashiram kahar <156418698+harshita194@users.noreply.github.com>
2025-07-08 04:54:31 +0530

    Time: 16 ms (86.46%), Space: 139.2 MB (60.28%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
+        int maxi = INT_MIN;
         vector<int> ans;
         deque<int> dq;
-        int maxi = INT_MIN;
-
         for(int i=0; i<n; i++){
-            // Remove elements from the front if they are out of the current window
-            if(!dq.empty() && dq.front() <= i-k){
-                dq.pop_front();
-            }
-            // Remove all elements smaller than the current from the back
-            while(!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
-
-            dq.push_back(i);
-
-            // Start recording the result once we have at least k elements
-            if(i >= k - 1) {
-                ans.push_back(nums[dq.front()]);
-            }
-
+            while(!dq.empty() && dq.front() <= i-k){
+            dq.pop_front();
+        }
+        while(!dq.empty() && nums[dq.back()] < nums[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if(i >= k-1){
+            ans.push_back(nums[dq.front()]);
+        }
         }
         return ans;
     }
